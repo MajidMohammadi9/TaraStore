dfe6183a542b00d658edc0d75827a5919b49c01a
Majid Mohammadi <majidmohammadi7@hotmail.com>
2025-01-06 10:26:31 +0400

    add forms to accounts

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import AdminUserCreationForm
+from django.contrib.auth import get_user_model
+
+# from .models import CustomUser
+
+class CustomUserCreationForm(AdminUserCreationForm):
+    class Meta:
+        model=get_user_model()
+        # model=CustomUser
+        fields=('email','username',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model=get_user_model()
+        fields=('email','username',)
