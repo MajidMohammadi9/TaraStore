6afd1630d149bd6d3703fbbbd99b6c34eaf7efca
Majid Mohammadi <majidmohammadi7@hotmail.com>
2025-01-06 10:16:30 +0400

    accounts app and CustomUser model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # local apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "tarastore",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "mypassword",
     }
 }
 
@@ -121,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL='accounts.CustomUser'
