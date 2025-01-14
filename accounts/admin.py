6c51976c465973954b244632dfd15fc8214897ae
Majid Mohammadi <majidmohammadi7@hotmail.com>
2025-01-15 00:15:47 +0400

    add customer and address model to accounts, add signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Customer, Address
 
 
 @admin.register(CustomUser)
@@ -22,3 +22,30 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
    
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['first_name', 'last_name', 'email']
+    list_per_page=10
+    search_fields=['user__last_name__istartwith', 'user__first_name__istartwith']
+    ordering=['user__last_name', 'user__first_name']
+
+    def first_name(self, customer):
+        return customer.user.first_name
+    
+    def last_name(self, customer):
+        return customer.user.last_name
+    
+    def email(self, customer):
+        return customer.user.email
+    
+    
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display=['full_name', 'street', 'city', 'province']
+    list_per_page=10
+    search_fields=['province', 'city', 'street']
+
+    def full_name(self, address):
+        return address.customer.full_name
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUser(AbstractUser):
-    email=models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    phone_number = PhoneNumberField(region="OM", blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @property
+    def full_name(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
+class Address(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    province = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.street}, {self.city}, {self.country}'
+    
